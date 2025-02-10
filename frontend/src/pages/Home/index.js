5fd76b3c83a6aea2630f3ffecb0abede1e3c1afa
Clara Meirelles <clara_meirelles@hotmail.com>
2025-02-09 23:36:07 -0300

    criação do hook para requisição de dados e base url

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,37 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { HomeContainer, HomeControls, HomeLayout } from './styled';
 import { Card } from '../../components/Card';
-
+import { useFetch } from '../../hooks/useFetch'
 export default function Home({ changePage }) {
     const [properties, setProperties] = useState([])
     const [cities, setCities] = useState([])
-
-    const fetchData = () => {
-        fetch("http://localhost:5000/acomodacoes")
-            .then((res) => res.json())
-            .then((data) => {
-                setProperties(data)
-                const citiesList = Array.from(
-                    new Set(data.map(property => property.localizacao))
-                );
-                setCities(citiesList)
-            })
-            .catch(() => alert("Erro ao buscar as acomodações. Por favor, recarregue a página."));
-    };
+    const [initialLoad, setInitialLoad] = useState(true)
+    const { data, err, fetchData } = useFetch("/acomodacoes", [])
 
     const filterAcomodationsByCity = (city) => {
         if (city) {
-            fetch(`http://localhost:5000/acomodacoes?cidade=${city}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    setProperties(data)
-                })
-                .catch(() => {
-                    alert("Erro ao buscar a cidade. Por favor, tente novamente mais tarde.")
-                    fetchData()
-                })
+            fetchData(`/acomodacoes?cidade=${city}`)
         } else {
-            fetchData()
+            fetchData("/acomodacoes")
         }
     }
 
@@ -41,8 +22,21 @@ export default function Home({ changePage }) {
     }
 
     useEffect(() => {
-        fetchData()
-    }, []);
+        setProperties(data)
+    }, [data]);
+
+    useEffect(() => {
+        if (initialLoad && properties.length > 0) {
+            setProperties(data);
+            const citiesList = Array.from(
+                new Set(data.map(property => property.localizacao))
+            );
+            setCities(citiesList);
+            setInitialLoad(false);
+        }
+    }, [properties]);
+
+    if (err) alert(err.erro, err.id);
 
     return (
         <HomeContainer>
